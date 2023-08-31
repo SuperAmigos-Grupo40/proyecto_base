445148528e131f87d46628c47add4929b6656837
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-31 15:22:29 -0300

    Modificación nombre modelo Tarea

---
+++ back/models/tarea-model.js
@@ -0,0 +1,62 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Tarea extends Model {
+    static associate(models) {
+      Tarea.belongsToMany(models.Volunteer, { through: models.TareasMid });
+    }
+  }
+  Tarea.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      id_coordinator: {
+        type: DataTypes.INTEGER,
+      },
+      id_volunteer: {
+        type: DataTypes.INTEGER,
+      },
+      point: {
+        type: DataTypes.BIGINT,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
+      place: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      id_category: {
+        type: DataTypes.INTEGER,
+      },
+      cant_participantes: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+    }, {
+      sequelize,
+      modelName: 'Tarea',
+      timestamps: false,
+      underscored: false,
+      createdAt: false,
+      updatedAt: false,
+    },
+  );
+  return Tarea;
+};
