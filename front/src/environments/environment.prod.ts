d607acfdcb51d0583021e57096c1602d7d0812ad
Rodrigo Arguello <109825448+rodriarguello@users.noreply.github.com>
2023-12-21 17:18:01 -0300

    Update environment.prod.ts

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://proyectobase-production-19e0.up.railway.app/'
+  API_URL: 'https://proyectobase-production-19e0.up.railway.app/'
 };
